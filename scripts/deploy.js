37e6b877cd9e553803229abe3e739c002e19d6b5
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2024-08-16 15:37:09 +0530

    lint: fixed styles of deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,11 +6,15 @@ async function main() {
   const { forwarder } = await hre.ignition.deploy(ForwarderModule);
   const forwarderAddress = await forwarder.getAddress();
   console.log("Deployed Forwarder Address: ", forwarderAddress);
-  const { registry } = await hre.ignition.deploy(FileverseZuPassRegistryModule, {
-    parameters: { FileverseZuPassRegistryModule: { trustedForwarder: forwarderAddress } },
-  });
+  const { registry } = await hre.ignition.deploy(
+    FileverseZuPassRegistryModule,
+    {
+      parameters: {
+        FileverseZuPassRegistryModule: { trustedForwarder: forwarderAddress },
+      },
+    }
+  );
   console.log(`Registry deployed to: ${await registry.getAddress()}`);
-
 }
 
 main().catch(console.error);
